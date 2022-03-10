7a9784896205418964539a51cbb8de9cd64d8b23
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2022-03-10 17:55:57 +0100

    Pre-deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link'
 import { MainLayout } from '../components/layouts/MainLayout'
-// import styles from '../styles/globals.css'
 
 export default function HomePage() {
   return (
